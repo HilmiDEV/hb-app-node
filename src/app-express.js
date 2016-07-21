f7ff683cb5c26edb9f9e12dc5bf792a911504fc1
hbouallegue <hilmi.bouallegue@geomatys.com>
2016-07-21 16:02:19 +0200

    Handle multiple routes with the framework express

--- src/app-express.js
+++ src/app-express.js
@@ -1,13 +1,29 @@
 // Load the module express
 var express = require("express");
 
+//create an object app
 var app = express();
 
 // Manage the routes of the app
-app.get('/',function(req,res){
+app
+    .get('/',function(req,res){
     //Define the header of the response
     res.setHeader("Content-Type","text/plain");
     res.end("Hilmi NodeJS & Express");
+    }).get("/hb",function(req,res){
+        res.setHeader("Content-Type","text/plain");
+        res.end("Hello The Big Boss Hilmi");
+    }).get("/hb/:n",function(req,res){
+    res.setHeader("Content-Type","text/plain");
+    // Get the parm from the req.params.param_name
+    res.end("Hello you want " + req.params.n);
+});
+
+
+// For the other routes not exist in the app we can define error page
+app.use(function(req,res){
+    res.setHeader("Content-Type","text/plain");
+    res.send(404,"Page not found");
 });
 
 // Listen to port
