df60156a11f3e894fa8d807f17818b07ad605602
hbouallegue <hilmi.bouallegue@geomatys.com>
2016-07-25 10:47:25 +0200

    Add the todolist

--- src/app-todolist/app-todo.js
+++ src/app-todolist/app-todo.js
@@ -1,40 +1,48 @@
-// ToDO List app with Express.js
-
-var express = require("express");
-
-var session = require("cookie-session"); // Load the session middleware
-
-var bodyParser = require("body-parser"); // Load the params manager middleware
-
-var urlencodedParser = bodyParser.urlencoded({extended : false});
+var express = require('express');
+var session = require('cookie-session'); // Charge le middleware de sessions
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var app = express();
 
-// Use the session middleware
-app.use(session({secret:"todotopsecret"}));
 
-app.get("/", function(req,res){
-    // Handle the get request
-    res.setHeader("Content-Type","text/html");
-    res.render("ToDo.ejs",{list:[]});
+/* On utilise les sessions */
+app.use(session({secret: 'todotopsecret'}))
 
-})
-    .post("/add",function(req,res){
-        //Handle the post request
-        
+
+/* S'il n'y a pas de todolist dans la session,
+ on en crée une vide sous forme d'array avant la suite */
+    .use(function(req, res, next){
+        if (typeof(req.session.todolist) == 'undefined') {
+            req.session.todolist = [];
+        }
+        next();
     })
-    .get("/delete/:id", function (req,res
-    ) {
-        
-    });
 
-app.listen(1111);
+    /* On affiche la todolist et le formulaire */
+    .get('/todo', function(req, res) {
+        res.render('ToDo.ejs', {todolist: req.session.todolist});
+    })
 
+    /* On ajoute un élément à la todolist */
+    .post('/todo/ajouter/', urlencodedParser, function(req, res) {
+        if (req.body.newtodo != '') {
+            req.session.todolist.push(req.body.newtodo);
+        }
+        res.redirect('/todo');
+    })
 
-/*
+    /* Supprime un élément de la todolist */
+    .get('/todo/supprimer/:id', function(req, res) {
+        if (req.params.id != '') {
+            req.session.todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    })
 
+    /* On redirige vers la todolist si la page demandée n'est pas trouvée */
+    .use(function(req, res, next){
+        res.redirect('/todo');
+    })
 
- new ol.layer.Vector({
- source: new ol.source.OSM()
- })
- */
+    .listen(8080);
