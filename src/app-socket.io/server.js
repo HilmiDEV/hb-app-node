d57c56c6df080a9a65e35d1855732dd51b819174
hbouallegue <hilmi.bouallegue@geomatys.com>
2016-07-29 15:43:51 +0200

    add the session variable to know the client pseudo name

--- src/app-socket.io/server.js
+++ src/app-socket.io/server.js
@@ -13,23 +13,40 @@ var fs =require("fs");
     });
  });
 
-// The part of handle the socket.io for the synchronize app with the client app
+    // The part of handle the socket.io for the synchronize app with the client app
 
-// Chargement de socket.io
-var io = require("socket.io").listen(server);
+    // Chargement de socket.io
+    var io = require("socket.io").listen(server);
 
-io.sockets.on('connection',function(socket){
-    console.log("un client est connecté !");
-});
+    // io.sockets.on('connection',function(socket){
+    // console.log("un client est connecté !");
+    // });
 
-io.sockets.on("connection", function(socket){
+    io.sockets.on("connection", function(socket){
+
     socket.emit("message","Vous êtes bien connecté");
+    /*
+    Envoyer un broadcast pour tous les autres clients connectés,
+           afin d'informer qu'il ya un nouveau client
+      */
+    socket.broadcast.emit("message","Un nouveau client est connecté");
+
     socket.on("message",function(msg){
-        console.log("Client : "+msg);
+        console.log("Client "+socket.pseudo+" : "+msg);
     });
-})
+        
+     socket.on("pseudo-msg",function (pseudo) {
+         socket.pseudo = pseudo;
+         console.log(socket.pseudo);
+         
+     })
 
 
+
+
+
+
+    });
 
 
 server.listen(1111);
